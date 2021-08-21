6dd6b46de27aabec6c955f66aeee4ff796321408
Aaditya <bluebird1339@gmail.com>
2021-08-21 19:53:52 -0400

    added format code

--- slack/msg.go
+++ slack/msg.go
@@ -1,8 +1,12 @@
 package slack
 
 import (
+	"context"
 	"log"
 	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
@@ -10,11 +14,43 @@ import (
 )
 
 func HandleMsgs(ev *slackevents.MessageEvent, client *socketmode.Client, api *slack.Client) {
+	var user string
+	var txt string
+	var userID string
+	var time string
 	backlinks := getBacklinks(ev.Text)
+	txt = ev.Text
+	userID = ev.User
+	time = ev.TimeStamp
 
-	log.Println(ev.Message)
+	if ev.ThreadTimeStamp != "" {
+		log.Println("thread")
+		params := &slack.GetConversationRepliesParameters{
+			Timestamp: ev.ThreadTimeStamp,
+			ChannelID: ev.Channel,
+		}
+		msgs, _, _, err := api.GetConversationReplies(params)
+		if err != nil {
+			log.Println("err", err)
+			return
+		}
+		txt = msgs[0].Text
+		userID = msgs[0].User
+		time = msgs[0].Timestamp
+	}
+
+	u, err := api.GetUserInfoContext(context.Background(), userID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	user = u.Profile.RealName
+	timeS, err := convertTime(time)
 	log.Println(backlinks)
-
+	log.Println(txt)
+	log.Println(user)
+	log.Println(time)
+	log.Println(timeS.UTC())
 }
 
 func getBacklinks(msg string) []string {
@@ -32,4 +68,14 @@ func GetTeamName(api *slack.Client) (string, error) {
 		return "", err
 	}
 	return resp.Team, nil
+}
+
+func convertTime(ut string) (time.Time, error) {
+	uts := strings.Split(ut, ".")
+	s, err := strconv.Atoi(uts[0])
+	ns, err := strconv.Atoi(uts[1])
+	if err != nil {
+		return time.Time{}, err
+	}
+	return time.Unix(int64(s), int64(ns)), nil
 }
