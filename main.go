4482a9827d0f55b76fb27d4f371765626eaefca5
LunarCoffee <lunarcoffee.pjc@gmail.com>
2021-08-21 23:00:37 -0400

    db stfuf

--- main.go
+++ main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	if err := InitDB(false); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	//DropAllTables()
+
+	//if err := AddWorkspace("nag"); err != nil {
+	//	fmt.Println(err)
+	//	panic(err)
+	//}
+
+	//err := AddBacklinkToWorkspace("nag", Backlink{LinkName: "bushan", NotionID: "nagabushan"})
+	//if err != nil {
+	//	fmt.Println(err)
+	//	panic(err)
+	//}
+
+	workspace := GetWorkspaceInfo("nag")
+	fmt.Println(workspace)
+
+	defer func() {
+		if err := DeinitDB(); err != nil {
+			fmt.Println(err)
+		}
+	}()
 }
